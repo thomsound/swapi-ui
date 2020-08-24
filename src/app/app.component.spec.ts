518ee8a73b882bbf9f258bcddd2938eed9620b2c
Carlo Thomsen <post@cpthomsen.de>
2020-08-24 21:05:02 +0200

    tidy up and push dist folder

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'assecor-fe'`, () => {
+  it(`should have as title 'star-wars-items'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('assecor-fe');
+    expect(app.title).toEqual('star-wars-items');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('assecor-fe app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('star-wars-items app is running!');
   });
 });
